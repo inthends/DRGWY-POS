ff294d85d9b4136ab8642364c774e5a2f65c5160
inthends <87696974@qq.com>
2020-11-07 14:37:09 +0800

    POS版本

---
+++ src/pages/building/building_service.js
@@ -0,0 +1,29 @@
+import api from '../../utils/api';
+// import common from '../../utils/common';
+
+export default {
+    getStatistics(pageIndex, OrganizeId,showLoading=true) {
+        return api.postData('/api/MobileMethod/MGetStatistics', {
+            pageIndex,
+            pageSize: 10,
+            sidx: 'name',
+            sord: 'asc',
+            OrganizeId,
+        },showLoading);
+    },
+    getStatisticsTotal(OrganizeId) {
+        return api.postData('/api/MobileMethod/MGetStatisticsTotal', {OrganizeId});
+    },
+
+    roomDetail(keyValue) {
+        return api.getData('/api/MobileMethod/MGetRoomEntity', {keyValue});
+    },
+    getUserInfo() {
+        return api.getData('/api/MobileMethod/MGetUserInfo',{},false);
+    },
+
+    logout() {
+        return api.postData('/api/MobileMethod/MLogout', {}, false);
+    }
+
+};
